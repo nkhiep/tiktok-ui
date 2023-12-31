bf0746d5bbd363f5c84bc186703471e12fa5a18c
hieppc <hieppc@gmail.com>
2023-12-31 21:35:28 +0700

    Restructuring and optimization #2 - PropTypes

--- src/components/Propper/Menu/index.js
+++ src/components/Propper/Menu/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import Tippy from '@tippyjs/react/headless';
 import { Wrapper as ProperWrapper } from '~/components/Propper';
 import MenuItem from './MenuItem';
@@ -46,7 +47,7 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
                     <ProperWrapper className={cx('menu-proper')}>
                         {history.length > 1 && (
                             <Header
-                                title="Language"
+                                title={current.title}
                                 onBack={() => {
                                     setHistory((prev) => prev.slice(0, prev.length - 1));
                                 }}
@@ -62,5 +63,12 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
         </Tippy>
     );
 }
+
+Menu.propTypes = {
+    children: PropTypes.node.isRequired,
+    items: PropTypes.array,
+    hideOnClick: PropTypes.bool,
+    onChange: PropTypes.func,
+};
 
 export default Menu;
